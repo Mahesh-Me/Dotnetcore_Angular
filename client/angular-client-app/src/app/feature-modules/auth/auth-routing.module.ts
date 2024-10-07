5caa54b2d56128486935df1f7a9e903aa7cc658d
Mahesh-Me <rathmahesh84@gmail.com>
2024-10-07 10:05:50 +0530

    mahesh -- bg image and routing added. Log in component added

--- client/angular-client-app/src/app/feature-modules/auth/auth-routing.module.ts
+++ client/angular-client-app/src/app/feature-modules/auth/auth-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AuthRoutingModule {
-  static components = [];
+  static components = [
+    LoginComponent
+  ];
  }
 
