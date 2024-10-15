fc8d9e9a2c4290c6e217ef4230690dbb0ac872fd
Mahesh-Me <rathmahesh84@gmail.com>
2024-10-15 14:04:43 +0530

    mahesh -- dbcontext configured and sql server integrated

---
+++ server/InventoryApp/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region Register DbSet
+
+        #endregion
+    }
+}
