a09680906e703737bf34ac3d0f0456931448e69b
Mahesh-Me <rathmahesh84@gmail.com>
2024-10-21 15:00:02 +0530

    header and dashboard page work started

--- client/angular-client-app/src/app/feature-modules/services/services-routing.module.ts
+++ client/angular-client-app/src/app/feature-modules/services/services-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: '',
     component:ServicesComponent,
-    canActivate: [AuthGuard],
+    //canActivate: [AuthGuard],
     children:[
       {
         path:'dashboard',
